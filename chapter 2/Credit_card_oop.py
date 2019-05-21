56e4655cdfefb493bdad7910e810274934a47834
Ashish Gupta <ashishgupta2692@gmail.com>
2019-05-22 00:05:12 +0530

    Add files via upload

--- chapter 2/Credit_card_oop.py
+++ chapter 2/Credit_card_oop.py
@@ -64,5 +64,15 @@ if __name__ =='__main__':
 		wallet[1].charge(2*val)
 		wallet[2].charge(3*val)
 
-	
+	for c in range(3):
+		print('customer =',wallet[c].get_customer())
+		print('Bank =',wallet[c].get_bank())
+		print('Account =',wallet[c].get_account())
+		print('Limit =',wallet[c].get_limit())
+		print('Balance =',wallet[c].get_balance())
+
+		while wallet[c].get_balance() > 100:
+			wallet[c].make_payement(100)
+			print('New balance =',wallet[c].get_balance())
+		print()
 
