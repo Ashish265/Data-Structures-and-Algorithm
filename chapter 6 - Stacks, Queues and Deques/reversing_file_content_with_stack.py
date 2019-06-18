6fb8b0b489a69a689bd39e0cb52a7247f53705eb
Ashish Gupta <ashishgupta2692@gmail.com>
2019-06-18 19:47:37 +0530

    Add files via upload

---
+++ chapter 6 - Stacks, Queues and Deques/reversing_file_content_with_stack.py
@@ -0,0 +1,18 @@
+from stack import ArrayStack
+
+def reverse_file(filename):
+	""" overwrite given file with its contents line by line reversed """
+	S = ArrayStack()
+
+	original = open(filename)
+	for line in original:
+		S.push(line.rstrip('\n'))
+	original.close()
+
+	output = open(filename,"w")
+
+	while not S.is_empty():
+		output.write(S.pop()+"\n")
+	output.close()
+
+
