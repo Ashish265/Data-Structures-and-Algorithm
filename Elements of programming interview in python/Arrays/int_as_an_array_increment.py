7b0b1567e9e95b24d66a117f5afb0aa1a08c5589
Ashish Gupta <ashishgupta2692@gmail.com>
2019-07-30 22:43:35 +0530

    Add files via upload

---
+++ Elements of programming interview in python/Arrays/int_as_an_array_increment.py
@@ -0,0 +1,21 @@
+def plus_one(A):
+	
+	A[-1] += 1
+
+	for i in reversed(range(1,len(A))):
+		if A[i] !=10:
+			break
+
+		A[i] = 0
+		A[i-1] += 1
+
+	if A[0] == 10:
+		A[0] = 1
+		A.append(0)
+
+	return A
+
+
+z = [1,5,9]
+print(plus_one(z))
+
