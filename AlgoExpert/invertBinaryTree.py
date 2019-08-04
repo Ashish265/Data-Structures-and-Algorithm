657c0b0838b0c726a6e1d62068c0de94d290dd6a
Ashish Gupta <ashishgupta2692@gmail.com>
2019-08-05 00:22:15 +0530

    Add files via upload

---
+++ AlgoExpert/invertBinaryTree.py
@@ -0,0 +1,11 @@
+def invertBinaryTree(tree):
+    # Write your code here.
+	
+	if tree is None:
+		return
+	swapLeftAndRight(tree)
+	invertBinaryTree(tree.left)
+	invertBinaryTree(tree.right)
+	
+def swapLeftAndRight(tree):
+	tree.left,tree.right = tree.right,tree.left
